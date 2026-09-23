6f9b2dc53fc30b9885333fc81082784e2da60143
Alexandre Perez <perezalexandre430@gmail.com>
2026-09-23 14:07:36 +0200

    Ajout de la configuration de l'application avec Spring Boot, y compris les fichiers de configuration, les contrôleurs, la sécurité et les dépendances nécessaires.

---
+++ back/src/test/java/fr/election/api/ElectionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.election.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
