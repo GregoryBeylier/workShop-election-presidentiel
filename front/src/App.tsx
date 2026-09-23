df6f15626fc1b5e9c5a345230e3f619da3860761
Gregory <samsung261101@gmail.com>
2026-09-23 11:21:33 +0200

    Add React Router and set-password page

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,16 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
+import SetPassword from "./components/SetPassword/SetPassword";
 
 function App() {
-  return <Login />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/set-password" element={<SetPassword />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
